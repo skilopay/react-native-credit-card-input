24d15379c2b370a4d352a54236a56f2f163f72ae
Cássio Santos <sss.cassio@gmail.com>
2019-09-18 19:12:50 -0300

    :construction: Start Type definition

---
+++ index.ts
@@ -0,0 +1,21 @@
+import * as React from 'react';
+
+export interface CreditCardInputProps {
+  cardImageFront: any;
+  cardImageBack: any;
+  fontFamily: any;
+  cardScale: any;
+  autoFocus: any;
+  fieldsOrder: any;
+  requiresName: any;
+  expiryLabel: any;
+  inputContainerStyle: any;
+  labels: any;
+  onFocus: any;
+  viewPlaceholder: any;
+  placeholders: any;
+}
+
+export class CreditCardInput extends React.Component<CreditCardInputProps> {
+  focus(string): void;
+}
